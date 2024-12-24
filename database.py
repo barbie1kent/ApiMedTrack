18e1c339d47636a1cc0b764fbaf63064a16ea24f
barbie1kent <147564337+barbie1kent@users.noreply.github.com>
2024-12-24 18:18:42 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,90 @@
+from sqlalchemy import create_engine, text, exc
+from sqlalchemy.orm import sessionmaker, relationship, DeclarativeBase
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Time
+
+class Base(DeclarativeBase):
+    pass
+
+class Gender(Base):
+    __tablename__ = 'gender'
+
+    id = Column(Integer, primary_key=True, index=True)
+    userGender = Column(String(15), nullable=False)
+
+    users = relationship("User", back_populates="gender")
+
+class ReseptionRate(Base):
+    __tablename__ = 'reseptionRate'
+
+    id = Column(Integer, primary_key=True, index=True)
+    rate = Column(String, nullable=False)
+
+    medicines = relationship("Medicine", back_populates="reseptionRate")
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String(20), nullable=False)
+    password = Column(String(10), nullable=False)
+    genderId = Column(Integer, ForeignKey('gender.id'), nullable=False)
+
+    gender = relationship("Gender", back_populates = "users")
+    medicines = relationship("Medicine", back_populates = "user")
+    medicineSchedules = relationship("MedicineSchedule", back_populates = "user")
+
+class Medicine(Base):
+    __tablename__ = 'medicine'
+
+    id = Column(Integer, primary_key=True, index=True)
+    dose = Column(String(30), nullable=False)
+    reseptionRateId = Column(Integer, ForeignKey('reseptionRate.id'), nullable=False)
+    takingTime = Column(Time, nullable=False)
+    storageLocation = Column(String(30), nullable=False)
+    additionalInformation = Column(String(100), nullable=False)
+    medicineDictionaryId = Column(Integer, ForeignKey('medicineDictionary.id'), nullable=False)
+    userId = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    reseptionRate = relationship("ReseptionRate", back_populates = "medicines")
+    user = relationship("User", back_populates = "medicines")
+    medicineDictionary = relationship("MedicineDictionary", back_populates= "medicines")
+    medicineSchedules = relationship("MedicineSchedule", back_populates= "medicine")
+
+class MedicineSchedule(Base):
+    __tablename__ = 'medicineSchedule'
+
+    id = Column(Integer, primary_key=True, index=True)
+    dose = Column(Numeric(10, 2), nullable=False)
+    takingTime = Column(Time, nullable=False)
+    frequency = Column(String(10), nullable=False)
+    userId = Column(Integer, ForeignKey('user.id'), nullable=False)
+    medicineId = Column(Integer, ForeignKey('medicine.id'), nullable=False)
+    unitMeasurementId = Column(Integer, ForeignKey('unitMeasurement.id'), nullable=False)
+
+    user = relationship("User", back_populates="medicineSchedules")
+    medicine = relationship("Medicine", back_populates="medicineSchedules")
+    unitMeasurement = relationship("UnitMeasurement", back_populates="medicineSchedules")
+
+class UnitMeasurement(Base):
+    __tablename__ = 'unitMeasurement'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(20), nullable=False)
+
+    medicineSchedules = relationship("MedicineSchedule", back_populates="unitMeasurement")
+
+class MedicineDictionary(Base):
+    __tablename__ = 'medicineDictionary'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+
+    medicines = relationship("Medicine", back_populates="medicineDictionary")
+
+
+engine = create_engine("postgresql://postgres:51650@localhost/MedTrack", echo=True)
+
+
+Base.metadata.create_all(bind=engine)
+
+
---
+++ main.py
@@ -0,0 +1,119 @@
+from fastapi import FastAPI, Depends, HTTPException, status, Body
+from sqlalchemy.orm import Session, sessionmaker
+from typing import List
+from database import *
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import joinedload
+from sqlalchemy import create_engine, text, exc
+from datetime import time
+
+
+SessionLocal = sessionmaker(engine, expire_on_commit=False)
+
+app = FastAPI()
+
+class UserLogin(BaseModel):
+    login: str
+    password: str
+
+class UserReg(BaseModel):
+    login: str
+    password: str
+    genderId: int
+
+class AddMedicineNameRequest(BaseModel):
+    name: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/login", status_code=status.HTTP_200_OK)
+async def login(user_login: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.login == user_login.login, User.password == user_login.password).first()
+
+    if db_user is None:
+        raise HTTPException(status_code=401, detail="Invalid login or password")
+
+    return {
+        "id": db_user.id,
+        "login": db_user.login,
+        "genderId": db_user.genderId
+    }
+
+@app.post("/register", response_model=UserReg, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserReg, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.login == user.login).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Login already registered")
+
+    try:
+        new_user = User(login=user.login, password=user.password, genderId=user.genderId)  # Пароль хранится в открытом виде!
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+    except exc.IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Database error: {e}")
+
+'''@app.post("/add_medicine/", response_model=AddMedicineRequest, status_code=201)
+def add_medicine(medicine: AddMedicineRequest, db: Session = Depends(get_db)):
+    try:
+        new_medicine = Medicine(name=medicine.name, dose=medicine.dose, reseptionRateId=medicine.reseptionRateId,
+                                takingTime=medicine.takingTime, storageLocation=medicine.storageLocation,
+                                additionalInformation=medicine.additionalInformation,
+                                medicineDictionaryId=medicine.medicineDictionaryId, userId=medicine.userId)
+        db.add(new_medicine)
+        db.commit()
+        db.refresh(new_medicine)
+        return medicine  # Возвращаем данные о добавленном лекарстве
+    except exc.IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Database error: {e}")
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Server error: {e}")'''
+
+
+@app.post("/add_medicine_name/", status_code=201)
+def add_medicine_name(medicine_name: AddMedicineNameRequest, db: Session = Depends(get_db)):
+    try:
+        new_medicine_name = MedicineDictionary(name=medicine_name.name)
+        db.add(new_medicine_name)
+        db.commit()
+        db.refresh(new_medicine_name)
+        return {"message": "Название лекарства добавлено", "id": new_medicine_name.id}
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Ошибка сервера: {e}")
+
+'''@app.post("/medicines/", response_model=dict, status_code=201)
+async def create_medicine(medicine: MedicineCreate, db: SessionLocal = Depends(get_db)):
+    try:
+        # Проверка существования пользователя
+        user_check = await db.execute(select(User).where(User.id == medicine.userId)).scalar_one_or_none()
+        if user_check is None:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        # Добавление лекарства
+        stmt = insert(Medicine).values(
+            dose=medicine.dose,
+            frequency=medicine.frequency,
+            takingTime=medicine.taking_time,
+            storageLocation=medicine.storage_location,
+            additionalInformation=medicine.additional_information,
+            userId=medicine.userId,
+            medicineDictionaryId=1
+        )
+        result = await db.execute(stmt)
+        await db.commit()
+        return {"message": "Medicine added successfully", "medicine_id": result.inserted_primary_key[0]}
+
+    except Exception as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")'''
